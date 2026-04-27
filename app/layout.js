e69694e94d3733d9353ae4da1aea27251b048856
villasenor1188-ui <villasenor1188@gmail.com>
2026-04-27 12:44:55 -0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'BRIDA Energy', description: 'Energía solar profesional' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
